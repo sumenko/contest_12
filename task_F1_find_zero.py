34b1d6336485342903ea6d6ea62d60665a85412b
Vladimir Sumenko <3760097@gmail.com>
2021-05-11 00:13:35 +0300

    12 sprint homework

--- task_F1_find_zero.py
+++ task_F1_find_zero.py
@@ -1,8 +1,3 @@
-import time
-
-from gen_data import generate
-
-
 def find_zeros(n, data):
     street = data.split()  # улица
     paths = []  # расстояния до нулей
@@ -35,11 +30,4 @@ if __name__ == '__main__':
     assert (find_zeros(12, '99 0 100 72 43 49 0 51 19 61 93 31') ==
             '1 0 1 2 2 1 0 1 2 3 4 5')
 
-    n = 10**6
-    print('Prepare data')
-    data = generate(n, 10**9, 40)
-    print('Start test')
-    start = time.time()
-    find_zeros(n, data)
-    print("time: {:.2f}".format(time.time() - start))
-    # print(find_zeros(int(input()), input()))
+    print(find_zeros(int(input()), input()))
