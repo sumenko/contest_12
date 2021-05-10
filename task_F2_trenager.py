a730ef864d8f1a462ffb9a58bd05c9fa1ddf242f
Vladimir Sumenko <3760097@gmail.com>
2021-05-11 00:10:45 +0300

    task_F2_trenager

--- task_F2_trenager.py
+++ task_F2_trenager.py
@@ -2,11 +2,25 @@ from f2_test import test_data
 
 
 def trainer(k, data):
-    
-    return 2
+    join_data = ''.join(data)
+
+    score = 0
+    keys = set(''.join(join_data.split('.')))
+    for key in keys:
+        count = join_data.count(key)
+        if count <= 2 * k:
+            score += 1
+
+    return score
 
 
 if __name__ == '__main__':
     for test in test_data.keys():
-        print('Test {}'.format(test))
-        assert trainer(test_data[test]['k'], test_data[test]['data']) == test_data[test]['answer']
+        assert (trainer(test_data[test]['k'], test_data[test]['data']) ==
+                test_data[test]['answer'])
+
+    k = int(input())
+    data = []
+    for i in range(4):
+        data += input()
+    print(trainer(k, data))
